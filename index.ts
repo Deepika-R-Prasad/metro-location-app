f13e53b2b7b10c3895eaa134c2a6ab952d06d726
Deepika R Prasad <71175035+Deepika-R-Prasad@users.noreply.github.com>
2026-08-16 01:19:09 +0530

    Define background task only once at module load

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import { defineBackgroundLocationTask } from './src/utils/backgroundLocationTask';
-
-defineBackgroundLocationTask();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// the environment is set up appropriately.
 registerRootComponent(App);
